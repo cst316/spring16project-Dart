8386b5e864810b10450a0e89958212b1da2b805c
Christopher Diaz <crdiaz3@asu.edu>
2016-02-17 23:43:50 -0700

    Added pop-up warning on close (X)

--- src/net/sf/memoranda/ui/App.java
+++ src/net/sf/memoranda/ui/App.java
@@ -3,6 +3,7 @@ package net.sf.memoranda.ui;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
+import java.awt.Point;
 import java.util.Calendar;
 
 import javax.swing.ImageIcon;
@@ -12,6 +13,8 @@ import javax.swing.UIManager;
 
 import net.sf.memoranda.EventsScheduler;
 import net.sf.memoranda.util.Configuration;
+import net.sf.memoranda.util.Local;
+import net.sf.memoranda.util.Context;
 
 /**
  *
